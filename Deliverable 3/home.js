bf02819f0b6e200d7cf7d2acf4f493b4eab97934
Achilles <a.sadsad@student.unsw.edu.au>
2021-04-06 23:21:45 +1000

    Added barebones for homepage

---
+++ Deliverable 3/home.js
@@ -0,0 +1,5 @@
+var index = document.getElementById("eventFeed"); 
+const test = document.createElement("img");
+test.src = "./test.jpg";
+test.setAttribute("class", "eventImage");
+index.appendChild(test);
