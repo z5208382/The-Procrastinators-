0b51c2d222de68ca719f4c30cddad94c5a8540a3
Achilles <a.sadsad@student.unsw.edu.au>
2021-04-07 00:52:04 +1000

    Buttons and looks good at 100%... dont look at it at anything else

--- Deliverable 3/home.js
+++ Deliverable 3/home.js
@@ -1,5 +1,12 @@
-var index = document.getElementById("eventFeed"); 
-const test = document.createElement("img");
-test.src = "./test.jpg";
-test.setAttribute("class", "eventImage");
-index.appendChild(test);
+var i = 0; 
+while(i < 20){
+    var index = document.getElementById("eventFeed"); 
+    const test = document.createElement("img");
+    test.src = "./test.jpg";
+    test.setAttribute("class", "eventImage");
+    index.appendChild(test);
+    i++; 
+}
+
+document.getElementById("Home Button").addEventListener("click", function(){alert("Hello World!");});
+
