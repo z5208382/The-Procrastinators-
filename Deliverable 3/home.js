ffbc44151e6d0106a8d57ab910fc555306ef8b37
Achilles <a.sadsad@student.unsw.edu.au>
2021-04-07 01:07:50 +1000

    Buttons Work

--- Deliverable 3/home.js
+++ Deliverable 3/home.js
@@ -4,9 +4,15 @@ while(i < 20){
     const test = document.createElement("img");
     test.src = "./test.jpg";
     test.setAttribute("class", "eventImage");
+    test.setAttribute("id", "eventImage");
     index.appendChild(test);
     i++; 
 }
 
 document.getElementById("Home Button").addEventListener("click", function(){alert("Hello World!");});
 
+var elements = document.getElementsByClassName("eventImage");
+
+for (var i = 0; i < elements.length; i++) {
+    elements[i].addEventListener('click', function(){alert("Hello World!");});
+}
